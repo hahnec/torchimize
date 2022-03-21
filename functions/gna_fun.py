294a6afed1426d94943d47969c6984d7adc3f122
hahnec <christopher.hahne@unibe.ch>
2022-03-21 15:52:50 +0100

    style(docs): add function description

--- functions/gna_fun.py
+++ functions/gna_fun.py
@@ -4,6 +4,16 @@ from functions.jacobian import jacobian_approx_t
 
 
 def lsq_gna(p, function, args=(), l=.1, tol=1e-7, max_iter=500):
+    """
+    Gauss-Newton implementation for least-squares fitting of non-linear functions
+    :param p: initial value(s)
+    :param function: user-provided function which takes p (and additional arguments) as input
+    :param args: optional arguments passed to function
+    :param l: learning rate
+    :param tol: tolerance for stop condition
+    :param max_iter: maximum number of iterations
+    :return: list of results, eps
+    """
 
     if len(args) > 0:
         fun_args_pos_wrapper = lambda args, p: function(p, args)
