fix(args): rectify partial function args while adding Jacobian function arg capability

tests/unit_test_skewed_gaussian.py
@@ -16,7 +16,7 @@ class SkewedGaussianTest(unittest.TestCase):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         self.initials = torch.tensor([7.5, -.5, .5, 2.5], dtype=torch.float64, device=self.device, requires_grad=True)
-        self.cost_fun = lambda p, y: (y-self.skewed_gaussian(p))**2
+        self.cost_fun = lambda p, y, placeholder: (y-self.skewed_gaussian(p))**2
 
         norm, mean, sigm, skew = 10, -1, 2, 5
         self.gt_params = torch.tensor([norm, mean, sigm, skew], dtype=torch.float64, device=self.device)
@@ -35,7 +35,7 @@ class SkewedGaussianTest(unittest.TestCase):
 
     def test_gna_skewed_gaussian(self):
 
-        coeffs, eps = lsq_gna(self.initials, self.cost_fun, args=(self.data_raw,), tol=1e-6)
+        coeffs, eps = lsq_gna(self.initials, self.cost_fun, args=(self.data_raw, None), tol=1e-6)
 
         # assertion
         ret_params = torch.allclose(coeffs[-1], self.gt_params, atol=1e-1)
@@ -45,7 +45,7 @@ class SkewedGaussianTest(unittest.TestCase):
 
     def test_lma_skewed_gaussian(self):
 
-        coeffs, eps = lsq_lma(self.initials, self.cost_fun, args=(self.data_raw,), meth='marq', tol=1e-6)
+        coeffs, eps = lsq_lma(self.initials, self.cost_fun, args=(self.data_raw, None), meth='marq', tol=1e-6)
 
         # assertion
         ret_params = torch.allclose(coeffs[-1], self.gt_params, atol=1e-1)

torchimize/functions/gna_fun.py
@@ -4,11 +4,12 @@ import functools
 from torchimize.functions.jacobian import jacobian_approx_t
 
 
-def lsq_gna(p, function, args=(), l=1., tol=1e-7, max_iter=500):
+def lsq_gna(p, function, jac_fun=None, args=(), l=1., tol=1e-7, max_iter=500):
     """
     Gauss-Newton implementation for least-squares fitting of non-linear functions
     :param p: initial value(s)
     :param function: user-provided function which takes p (and additional arguments) as input
+    :param jac_fun: user-provided Jacobian function which takes p (and additional arguments) as input
     :param args: optional arguments passed to function
     :param l: learning rate
     :param tol: tolerance for stop condition
@@ -17,12 +18,20 @@ def lsq_gna(p, function, args=(), l=1., tol=1e-7, max_iter=500):
     """
 
     if len(args) > 0:
-        fun_args_pos_wrapper = lambda args, p: function(p, args)
-        fun = functools.partial(fun_args_pos_wrapper, *args)
+        # swap parameters and arguments to enable functools.partial usage
+        fun_args_pos_wrapper = lambda args, p: function(p, *args)
+        fun = functools.partial(fun_args_pos_wrapper, args)
     else:
         fun = function
 
-    j = jacobian_approx_t(p, fun)
+    # use numerical Jacobian if analytical is not provided
+    if jac_fun is None:
+        jac_fun = functools.partial(jacobian_approx_t, f=fun)
+    else:
+        jac_args_pos_wrapper = lambda args, p: jac_fun(p, *args)
+        jac_fun = functools.partial(jac_args_pos_wrapper, args)
+
+    j = jac_fun(p)
     g = torch.matmul(j.T, fun(p))
     H = torch.matmul(j.T, j)
     eps = 1
@@ -31,7 +40,7 @@ def lsq_gna(p, function, args=(), l=1., tol=1e-7, max_iter=500):
         h = -l*torch.matmul(torch.linalg.inv(H), g)
         p = p + h
         p_list.append(p.detach())
-        j = jacobian_approx_t(p, fun)
+        j = jac_fun(p)
         g = torch.matmul(j.T, fun(p))
         H = torch.matmul(j.T, j)
         eps = max(abs(g))

torchimize/functions/lma_fun.py
@@ -5,11 +5,12 @@ import functools
 from torchimize.functions.jacobian import jacobian_approx_t
 
 
-def lsq_lma(p, function, args=(), tol=1e-7, tau=1e-3, meth='lev', rho1=.25, rho2=.75, bet=2, gam=3, max_iter=500):
+def lsq_lma(p, function, jac_fun=None, args=(), tol=1e-7, tau=1e-3, meth='lev', rho1=.25, rho2=.75, bet=2, gam=3, max_iter=500):
     """
     Levenberg-Marquardt implementation for least-squares fitting of non-linear functions
     :param p: initial value(s)
     :param function: user-provided function which takes p (and additional arguments) as input
+    :param jac_fun: user-provided Jacobian function which takes p (and additional arguments) as input
     :param args: optional arguments passed to function
     :param tol: tolerance for stop condition
     :param tau: factor to initialize damping parameter
@@ -23,12 +24,19 @@ def lsq_lma(p, function, args=(), tol=1e-7, tau=1e-3, meth='lev', rho1=.25, rho2
     """
 
     if len(args) > 0:
-        fun_args_pos_wrapper = lambda args, p: function(p, args)
-        fun = functools.partial(fun_args_pos_wrapper, *args)
+        fun_args_pos_wrapper = lambda args, p: function(p, *args)
+        fun = functools.partial(fun_args_pos_wrapper, args)
     else:
         fun = function
 
-    j = jacobian_approx_t(p, fun)
+    # use numerical Jacobian if analytical is not provided
+    if jac_fun is None:
+        jac_fun = functools.partial(jacobian_approx_t, f=fun)
+    else:
+        jac_args_pos_wrapper = lambda args, p: jac_fun(p, *args)
+        jac_fun = functools.partial(jac_args_pos_wrapper, args)
+
+    j = jac_fun(p)
     g = torch.matmul(j.T, fun(p))
     H = torch.matmul(j.T, j)
     u = tau * torch.max(torch.diag(H.diagonal()))
@@ -47,7 +55,7 @@ def lsq_lma(p, function, args=(), tol=1e-7, tau=1e-3, meth='lev', rho1=.25, rho2
         if rho > 0:
             p = p + h
             p_list.append(p.detach())
-            j = jacobian_approx_t(p, fun)
+            j = jac_fun(p)
             g = torch.matmul(j.T, fun(p))
             H = torch.matmul(j.T, j)
         if meth== 'lev':