1c2df55f36a26a0a50a2117c3b4c1e29f29d363f
hahnec <christopher.hahne@unibe.ch>
2022-05-12 16:34:38 +0200

    feat(pip): initial setup.py commit

---
+++ setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+__author__ = "Christopher Hahne"
+__email__ = "inbox@christopherhahne.de"
+__license__ = """
+    Copyright (c) 2022 Christopher Hahne <inbox@christopherhahne.de>
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from setuptools import setup, find_packages
+from torchimize import __version__
+from docutils import core
+import os
+
+OPTIONS = {
+    "argv_emulation": True,
+    "compressed": True,
+    "optimize": 2,
+    "excludes": ['matplotlib'],
+    "plist": dict(NSHumanReadableCopyright='2022 Christopher Hahne'),
+    "packages": ['torch'],
+}
+
+path = os.path.dirname(os.path.realpath(__file__))
+# parse description section text
+readme_path = os.path.join(path, 'README.rst')
+with open(readme_path, 'r') as f:
+    data = f.read()
+    readme_nodes = list(core.publish_doctree(data))
+    for node in readme_nodes:
+        if node.astext().startswith('Description'):
+                long_description = node.astext().rsplit('\n\n')[1]
+
+# parse package requirements from text file
+reqtxt_path = os.path.join(path, 'requirements.txt')
+with open(reqtxt_path, 'r') as f:
+    req_list = f.read().split('\n')
+
+setup(
+      name='torchimize',
+      version=__version__,
+      description='Optimization Algorithms using Pytorch',
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
+      url='http://github.com/hahnec/torchimize',
+      author='Christopher Hahne',
+      author_email='inbox@christopherhahne.de',
+      license='GNU GPL V3.0',
+      keywords='pytorch torch optimization mathematical linear programming gauss newton levenberg marquardt',
+      scripts=[''],
+      entry_points={'console_scripts': [''],},
+      packages=find_packages(),
+      install_requires=req_list,
+      include_package_data=True,
+      zip_safe=False,
+      )
