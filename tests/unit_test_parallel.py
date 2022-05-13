c399484c08f858ac5bc8812fdbe52fd00d8d4922
hahnec <christopher.hahne@unibe.ch>
2022-05-13 08:30:41 +0200

    feat(par): add initial parallel Gauss-Newton approach

---
+++ tests/unit_test_parallel.py
@@ -0,0 +1,136 @@
+__author__ = "Christopher Hahne"
+__email__ = "inbox@christopherhahne.de"
+__license__ = """
+    Copyright (c) 2022 Christopher Hahne <inbox@christopherhahne.de>
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import unittest
+import torch
+
+from torchimize.functions.gna_fun_parallel import lsq_gna
+from torchimize.functions.lma_fun_parallel import lsq_lma
+from tests.emg_mm import *
+
+class ParallelOptimizationTest(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(ParallelOptimizationTest, self).__init__(*args, **kwargs)
+
+    def setUp(self):
+        
+        torch.manual_seed(3008)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        # alpha, mu, sigma, eta
+        norm, mean, sigm, skew = 10, -1, 80, 5
+        self.initials = torch.tensor([7.5, -.75, 10, 3], dtype=torch.float64, device=self.device, requires_grad=True)
+        self.cost_fun = lambda p_batch, t, y: (y-self.emg_model_batch(p_batch, t))**2
+
+        self.gt_params = torch.tensor([norm, mean, sigm, skew], dtype=torch.float64, device=self.device)
+        self.t = torch.linspace(-1e3, 1e3, int(2e3)).to(self.device)
+        channel_num = 4
+        self.data_channels = []
+        self.initials_list = []
+        for i in range(channel_num):
+            torch.manual_seed(3008+i)
+            data = self.emg_model(self.gt_params, t=self.t)
+            self.data_channels.append(data + .01 * torch.randn(len(data), dtype=torch.float64, device=self.device))
+            self.initials_list.append(self.initials)
+        self.batch_data_channels = torch.stack(self.data_channels, dim=0)
+        self.batch_initials = torch.stack(self.initials_list, dim=0)
+
+    def emg_model_batch(
+            self,
+            p_batch,
+            t: torch.Tensor = None,
+        ):
+
+        emg_batch = []
+        for i in range(p_batch.shape[0]):
+
+            emg = self.emg_model(p_batch[i, ...], t=t)
+            emg_batch.append(emg)
+
+        return torch.stack(emg_batch, dim=0)
+
+    @staticmethod
+    def emg_model(
+            p,
+            t: torch.Tensor = None,
+        ):
+
+        alpha, mu, sigma, eta = p
+
+        alpha = 1 if alpha is None else alpha
+        gauss = gauss_term(t, mu, sigma)
+        asymm = asymm_term(t, mu, sigma, eta)
+
+        return alpha * gauss * asymm
+
+    def emg_jac_batch(self, p_batch, t=None, data=None):
+
+        emg_jac_batch = []
+        for i in range(p_batch.shape[0]):
+
+            emg_jac = self.emg_jac(p_batch[i, ...], t=t, data=data[i, ...])
+            emg_jac_batch.append(emg_jac)
+
+        return torch.stack(emg_jac_batch, dim=0)
+
+    def emg_jac(self, p, t=None, data=None):
+
+        alpha, mu, sigma, eta = p
+
+        c = 2*(data-self.emg_model([alpha, mu, sigma, eta], t=t))[..., None]
+
+        jacobian = c * torch.stack([
+            pd_emg_wrt_alpha(t, mu, sigma, eta),
+            alpha*pd_emg_wrt_mu(t, mu, sigma, eta),
+            alpha*pd_emg_wrt_sigma(t, mu, sigma, eta),
+            alpha*pd_emg_wrt_eta(t, mu, sigma, eta),
+        ]).T
+
+        return jacobian
+
+    def test_gna_emg(self):
+
+        coeffs = lsq_gna(self.batch_initials, self.cost_fun, jac_function=self.emg_jac_batch, args=(self.t, self.batch_data_channels), l=.1, gtol=1e-6, max_iter=199)
+
+        # assertion
+        ret_params = torch.allclose(coeffs[-1], self.gt_params, atol=1e-1)
+        self.assertTrue(ret_params, 'Coefficients deviate')
+        eps = torch.sum(self.cost_fun(coeffs[-1], t=self.t, y=self.batch_data_channels))
+        self.assertTrue(eps.cpu() < 1, 'Error exceeded 1')
+        self.assertTrue(len(coeffs) < 200, 'Number of iterations exceeded 200')
+
+    def _test_lma_emg(self):
+
+        for m in ['lev', 'marq']:
+
+            coeffs = lsq_lma(self.batch_initials, self.cost_fun, jac_function=self.emg_jac_batch, args=(self.t, self.batch_data_channels), meth=m, max_iter=39)
+
+            # assertion
+            ret_params = torch.allclose(coeffs[-1], self.gt_params, atol=1e-1)
+            self.assertTrue(ret_params, 'Coefficients deviate')
+            eps = torch.sum(self.cost_fun(coeffs[-1], t=self.t, y=self.batch_data_channels))
+            self.assertTrue(eps.cpu() < 1, 'Error exceeded 1')
+            self.assertTrue(len(coeffs) < 40, 'Number of iterations exceeded 40')
+
+    def test_all(self):
+
+        self.test_gna_emg()
+        #self.test_lma_emg()
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ torchimize/functions/gna_fun_parallel.py
@@ -0,0 +1,64 @@
+import torch
+from typing import Union, Callable, Tuple, List
+
+from torchimize.functions.jacobian import jacobian_approx_t
+
+def lsq_gna(
+        p: torch.Tensor,
+        function: Callable, 
+        jac_function: Callable = None,
+        args: Union[Tuple, List] = (),
+        ftol: float = 1e-8,
+        ptol: float = 1e-8,
+        gtol: float = 1e-8,
+        l: float = 1.,
+        max_iter: int = 100,
+    ) -> List[torch.Tensor]:
+    """
+    Gauss-Newton implementation for least-squares fitting of non-linear functions
+    :param p: initial value(s)
+    :param function: user-provided function which takes p (and additional arguments) as input
+    :param jac_fun: user-provided Jacobian function which takes p (and additional arguments) as input
+    :param args: optional arguments passed to function
+    :param ftol: relative change in cost function as stop condition
+    :param ptol: relative change in independant variables as stop condition
+    :param gtol: maximum gradient tolerance as stop condition
+    :param l: learning rate
+    :param max_iter: maximum number of iterations
+    :return: list of results, eps
+    """
+
+    if len(args) > 0:
+        # pass optional arguments to function
+        fun = lambda p: function(p, *args)
+    else:
+        fun = function
+
+    if jac_function is None:
+        # use numerical Jacobian if analytical is not provided
+        jac_fun = lambda p: jacobian_approx_t(p, f=fun)
+    else:
+        jac_fun = lambda p: jac_function(p, *args)
+
+    f = fun(p)
+    j = jac_fun(p)
+    g = torch.bmm(j.transpose(-2, -1), f.unsqueeze(-1))
+    H = torch.bmm(j.transpose(-2, -1), j)
+    p_list = [p]
+    while len(p_list) < max_iter:
+        h = -l*torch.bmm(torch.pinverse(H), g).squeeze(-1)
+        p = p + h
+        p_list.append(p.detach())
+        f = fun(p)
+        j = jac_fun(p)
+        g = torch.bmm(j.transpose(-2, -1), f.unsqueeze(-1))
+        H = torch.bmm(j.transpose(-2, -1), j)
+
+        # stop conditions
+        gcon = torch.max(abs(g)) < gtol
+        pcon = (h**2).sum()**.5 < ptol*(ptol + (p**2).sum()**.5)
+        fcon = ((fun(p_list[-2])-fun(p_list[-1]))**2).sum() < ((ftol*f)**2).sum()
+        if gcon or pcon or fcon:
+            break
+
+    return p_list
