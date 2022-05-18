18bde0cca9cba26d619192eb1b006fb1c133f98c
hahnec <christopher.hahne@unibe.ch>
2022-05-18 16:03:45 +0200

    refactor(fun): add lma_parallel to functions namespace

--- torchimize/functions/__init__.py
+++ torchimize/functions/__init__.py
@@ -1,3 +1,4 @@
 from torchimize.functions.gna_fun import lsq_gna
 from torchimize.functions.lma_fun import lsq_lma
 from torchimize.functions.gna_fun_parallel import lsq_gna_parallel, lsq_gna_parallel_plain
+from torchimize.functions.lma_fun_parallel import lsq_lma_parallel
